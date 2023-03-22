68a9956e826bcc6b49ff3b155df23995c17685eb
monyorojoseph <monyorojoseph@gmail.com>
2023-03-23 01:05:39 +0300

    droppped ml models folder

--- ml-models/one/setup/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from endpoints.urls import urlpatterns as endpoint_urlpatterns
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
-urlpatterns += endpoint_urlpatterns
